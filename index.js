c032c6436e72c755723d88b5c75e20127719fee3
Sarah Rubio <sarah@MacBook-Air-de-Sarah.local>
2020-08-20 14:21:24 +0200

    try connection bdd

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mysql = require ('mysql');
+const mysql = require ('mysql')
 
 let app = express()
 
-const connection = mysql.createConnection ({
+const con = mysql.createConnection ({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'meet_invest_bdd'
+  database: 'meet_invest_bdd',
 });
 
-connection.connect (() => {
-  console.log ('Connecté!');
-});
+// con.connect(function() {
+//     try {
+//         console.log("Connected!");
+//         con.query("CREATE DATABASE meet_invest_bdd", function (err, result) {
+//             console.log("Database created");
+//         });
+//     } catch(err) {
+//         console.log(err);
+//     }
+//  });
+
+// con.connect(function() {
+//     try {
+//         console.log("Connected!");
+//         var sql = "CREATE TABLE entrepreneurs (id INT AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), address VARCHAR(255), cp INT, city VARCHAR(255), country VARCHAR(255), mail VARCHAR(255), phone INT)";
+//         con.query(sql, function (err, result) {
+//             try {
+//                 console.log("Table created");
+//             } catch(err) {
+//                 if (err) throw err;
+//             }
+//         });    
+//     } catch(err) {
+//         if (err) throw err;
+//     } 
+// });
+
+app.listen(8080, () => {
+    console.log("serveur lancé")
+})
