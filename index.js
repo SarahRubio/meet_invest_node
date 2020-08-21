0c384052f9210c9ed6529354a39bdaa853dcd1e3
Sarah Rubio <sarah@air-de-sarah.home>
2020-08-21 15:06:13 +0200

    try fix bug connection

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require ('mysql')
+const express = require('express');
+const mysql = require('mysql');
 
-let app = express()
-
-const con = mysql.createConnection ({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'meet_invest_bdd',
+// create connection
+const db = mysql.createConnection({
+    host:'localhost', 
+    user: 'meet_invest_admin',
+    password: 'Invest_now'
 });
 
-con.connect(function() {
-    try {
-        console.log("Connected!");
-    } catch(err) {
-        console.log(err);
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
     }
- });
+    console.log('MySql Connected');
+});
 
-// con.connect(function() {
+const app = express();
+
+// Create db
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE meetinvestdb';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result)
+        res.send('database created...')
+    })
+})
+
+// db.connect(function() {
 //     try {
 //         console.log("Connected!");
 //         con.query("CREATE DATABASE meet_invest_bdd", function (err, result) {
@@ -30,7 +39,7 @@ con.connect(function() {
 //     }
 //  });
 
-// con.connect(function() {
+// db.connect(function() {
 //     try {
 //         console.log("Connected!");
 //         var sql = "CREATE TABLE entrepreneurs (id INT AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), address VARCHAR(255), cp INT, city VARCHAR(255), country VARCHAR(255), mail VARCHAR(255), phone INT, raison_sociale VARCHAR(255), siret INT, capital_social INT, ca INT)";
@@ -46,7 +55,7 @@ con.connect(function() {
 //     } 
 // });
 
-// con.connect(function() {
+// db.connect(function() {
 //     try {
 //         console.log("Connected!");
 //         var sql = "CREATE TABLE projets (id INT AUTO_INCREMENT PRIMARY KEY, titre VARCHAR(255), description TEXT, financial_needs INT, other_needs TEXT, img VARCHAR(255), logo VARCHAR(255))";
@@ -62,7 +71,7 @@ con.connect(function() {
 //     } 
 // });
 
-// con.connect(function() {
+// db.connect(function() {
 //     try {
 //         console.log("Connected!");
 //         var sql = "CREATE TABLE investisseur (id INT AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), address VARCHAR(255), cp INT, phone INT, country VARCHAR(255), mail VARCHAR(255), risk VARCHAR(255), amount-to-invest INT, profitability INT, why_invest VARCHAR(255))";
@@ -79,6 +88,6 @@ con.connect(function() {
 // });
 
 
-app.listen(8080, () => {
-    console.log("serveur lancé")
+app.listen('3000', () => {
+    console.log('server started on port 3000');
 })
